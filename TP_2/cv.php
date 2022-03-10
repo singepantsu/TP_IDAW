60ef304d9b8389b23267faa189d833a58b11959d
singepantsu <mekki99@hotmail.fr>
2022-03-10 21:11:32 +0100

    Commit du vendredi

--- TP_2/cv.php
+++ TP_2/cv.php
@@ -1,5 +1,19 @@
 <div class="prem-flex">
     <div class="sec-flex">
-        <h1>Mon CV</h1>
+    <h1 style="text-align: center;">Mon CV</h1>
+                    <h2>Profil</h2>
+                    <h4>Etudiant serieux et motivé passionné par les mathématiques</h4>
+                    <h2>Parcours scolaire</h2>
+                    <h3><u>IMT Nord-Europe</u></h3>
+                    <h4>Eleve ingenieur.</h4>
+                    <h3><u>Lycée St-Louis Paris</u></h3>
+                    <h4>CPGE de mathematiques.</h4>
+                    <h2>parcours professionnel</h2>
+                    <h3><u>Mission au Conseil Régional d'Ile-de-France</u></h3>
+                    <h4>Developpement d'une Application en low-code.</h4>
+                    <h3><u>Stage au sein de Querydine SAS</u></h3>
+                    <h4>Developpement de base de données.</h4>
+                    <h2>Loisirs et centres d'intérêts</h2>
+                    <h4>Pour plus de renseignements sur cette section, consultez :<a href="hobbies.html">Mes Hobbies</a></h4>
     </div>
 </div>
--- TP_2/template_footer.php
+++ TP_2/template_footer.php
@@ -1,5 +1,8 @@
 <footer>
-    © 2022 John Doe
+    <?php
+date_default_timezone_set('UTC');
+echo "© " . date('H:i m.d. Y') . " John Doe";
+?>
 </footer>
 </body>
 </html>
